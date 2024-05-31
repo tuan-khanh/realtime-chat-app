5691a9e2a0f5044d2f634f5ca6656e2cc8d71ab5
EAD\knguyen281 <khanhtuan.nguyen@dxc.com>
2024-05-31 12:08:34 +0700

    re implement layout for messages

--- client/src/app/messages/layout.tsx
+++ client/src/app/messages/layout.tsx
@@ -1,8 +1,84 @@
 "use client";
-import { Box } from "@mui/material";
+import { ISimpleConversation } from "@/components/conversation/ISimpleConservation";
+import {
+  Box,
+  Button,
+  IconButton,
+  InputBase,
+  Paper,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useImmer } from "use-immer";
+import { _conservations } from "./conversations";
+import SimpleConversationItem from "@/components/conversation/simple-conservation-item";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 
 function MessagesLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <Box className="h-screen">{children}</Box>;
+  const [conservations, setConservations] = useImmer(
+    [] as ISimpleConversation[]
+  );
+  const [selectedConservationId, setSelectedConservationId] = useState(-1);
+  React.useEffect(() => {
+    setConservations(_conservations);
+  }, [setConservations]);
+  return (
+    <Box className="h-screen flex">
+      <Stack
+        className="w-96 bg-white h-screen px-4 overflow-hidden"
+        spacing={2}
+      >
+        <Box className="pt-8 flex items-end justify-between">
+          <Typography variant="h5" className="font-semibold">
+            Conversations
+          </Typography>
+          <IconButton aria-label="Start new chat" className="text-primary">
+            <Tooltip title="New message">
+              <FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
+            </Tooltip>
+          </IconButton>
+        </Box>
+        <Box>
+          <Paper className="flex items-center w-full p-1 rounded-2xl overflow-hidden ">
+            <SearchRoundedIcon></SearchRoundedIcon>
+            <InputBase
+              id="filled-adornment-amount"
+              className="w-full flex-1 ml-1"
+              placeholder="Search in Messenger"
+            ></InputBase>
+          </Paper>
+        </Box>
+        <Box>
+          <Typography variant="subtitle1" className="font-semibold py-2">
+            Messages
+          </Typography>
+          <Box className="h-full">
+            {conservations.map((item, index) => (
+              <Button
+                variant="text"
+                key={`${item.id}${index}`}
+                onClick={() => setSelectedConservationId(item.id)}
+                className={
+                  "w-full justify-start hover:bg-slate-300" +
+                  (selectedConservationId === item.id ? " bg-slate-300" : "")
+                }
+              >
+                <SimpleConversationItem
+                  conservationItem={item}
+                  selected={selectedConservationId === item.id}
+                ></SimpleConversationItem>
+              </Button>
+            ))}
+          </Box>
+        </Box>
+      </Stack>
+      {children}
+    </Box>
+  );
 }
 
 export default MessagesLayout;
